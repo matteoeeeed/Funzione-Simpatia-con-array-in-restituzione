6b3af88248703a065f2cb66fd8a2b0b79000969d
matteoeeeed <95379963+matteoeeeed@users.noreply.github.com>
2023-02-05 11:01:52 +0100

    Create utils.js

---
+++ utils.js
@@ -0,0 +1,11 @@
+const array=[1,2,3];
+let lenght = array.length;
+function LivelloSimpatia(){
+
+for(let i=0;i<lenght;i++){
+console.log(array[i])  
+}
+  
+}
+
+module.exports = {luuu: LivelloSimpatia}
